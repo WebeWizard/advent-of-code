cc3436103ba5c22f524c170966f5a46d534cbfdf
WebeWizard <justin.leis@webewizard.com>
2022-12-24 16:25:28 -0700

    day 10

---
+++ src/day10.rs
@@ -0,0 +1,348 @@
+const INPUT_EXAMPLE: &str = "addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+
+const INPUT: &str = "noop
+noop
+addx 6
+addx -1
+noop
+addx 5
+addx 3
+noop
+addx 3
+addx -1
+addx -13
+addx 17
+addx 3
+addx 3
+noop
+noop
+noop
+addx 5
+addx 1
+noop
+addx 4
+addx 1
+noop
+addx -38
+addx 5
+noop
+addx 2
+addx 3
+noop
+addx 2
+addx 2
+addx 3
+addx -2
+addx 5
+addx 2
+addx -18
+addx 6
+addx 15
+addx 5
+addx 2
+addx -22
+noop
+noop
+addx 30
+noop
+noop
+addx -39
+addx 1
+addx 19
+addx -16
+addx 35
+addx -28
+addx -1
+addx 12
+addx -8
+noop
+addx 3
+addx 4
+noop
+addx -3
+addx 6
+addx 5
+addx 2
+noop
+noop
+noop
+noop
+noop
+addx 7
+addx -39
+noop
+noop
+addx 5
+addx 2
+addx 2
+addx -1
+addx 2
+addx 2
+addx 5
+addx 1
+noop
+addx 4
+addx -13
+addx 18
+noop
+noop
+noop
+addx 12
+addx -9
+addx 8
+noop
+noop
+addx -2
+addx -36
+noop
+noop
+addx 5
+addx 2
+addx 3
+addx -2
+addx 2
+addx 2
+noop
+addx 3
+addx 5
+addx 2
+addx 19
+addx -14
+noop
+addx 2
+addx 3
+noop
+addx -29
+addx 34
+noop
+addx -35
+noop
+addx -2
+addx 2
+noop
+addx 6
+noop
+noop
+noop
+noop
+addx 2
+noop
+addx 3
+addx 2
+addx 5
+addx 2
+addx 1
+noop
+addx 4
+addx -17
+addx 18
+addx 4
+noop
+addx 1
+addx 4
+noop
+addx 1
+noop
+noop";
+
+const SPECIAL_CYCLES: [usize; 6] = [20, 60, 100, 140, 180, 220];
+
+fn main() {
+    let mut clock: usize = 0;
+    let mut reg_x: isize = 1; // starts with value of 1
+
+    let mut next_special_index = 0;
+
+    let mut CRT = [','; 240];
+
+    let mut result: isize = 0;
+
+    for line in INPUT.lines() {
+        let (instr, val_str) = line.split_once(' ').unwrap_or_else(|| ("noop", "0"));
+
+        let val = val_str.parse::<isize>().unwrap();
+
+        let mut clock_update: usize = 0;
+
+        match instr {
+            "noop" => {
+                clock_update = 1;
+            }
+            "addx" => clock_update = 2,
+            _ => panic!("bad input!"),
+        }
+
+        clock += clock_update;
+
+        // update the CRT
+        for i in 0..clock_update {
+            if (reg_x - 1) <= (clock - i - 1) as isize % 40
+                && (clock - i - 1) as isize % 40 <= (reg_x + 1)
+            {
+                CRT[clock - i - 1] = '#';
+            }
+        }
+
+        // check special cycles for part 1
+        if next_special_index < SPECIAL_CYCLES.len() && clock >= SPECIAL_CYCLES[next_special_index]
+        {
+            dbg!(SPECIAL_CYCLES[next_special_index], reg_x);
+            result += SPECIAL_CYCLES[next_special_index] as isize * reg_x;
+            next_special_index += 1;
+        }
+
+        reg_x += val;
+    }
+
+    println!("Part 1 Result: {}", result);
+
+    println!("Part 2 CRT:");
+    for chunk in CRT.chunks(40) {
+        println!("{:?}", chunk);
+    }
+}
